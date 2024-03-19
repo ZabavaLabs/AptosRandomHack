6b0091a269b38d68f3074ad56968f1dec1729a4e
Daniel <dan.tgw@gmail.com>
2024-03-19 13:24:06 +0800

    Improved UI for tabs.

--- frontend/apps/nextjs-example/components/Tabs.tsx
+++ frontend/apps/nextjs-example/components/Tabs.tsx
@@ -17,7 +17,7 @@ const Tabs: React.FC<TabsProps> = ({ tabs, activeTab, setActiveTab }) => {
             {tabs.map((tab, index) => (
                 <button
                     key={index}
-                    className={`mr-4 px-4 py-2 ${activeTab === index ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
+                    className={`mr-4 px-12 py-2 text-2xl ${activeTab === index ? ' text-white border-b-blue-500 border-b-4 font-bold' : 'font-normal text-gray-400 border-b-gray-700 border-b-0'
                         }`}
                     onClick={() => setActiveTab(index)}
                 >
